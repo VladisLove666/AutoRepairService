634f77c6bcd3938819c39d498a935c6711ea6d77
222114 <222114@B42E99861EB11>
2025-03-18 17:00:57 +0300

    Добавьте файлы проекта.

---
+++ AutoRepairService/Pages/LoginPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoRepairService.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text.Trim();
+            string password = PasswordBox.Password;
+
+            try
+            {
+                var user = App.db.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+                if (user != null)
+                {
+                    MessageBox.Show($"Добро пожаловать, {user.Username}!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                    var role = App.db.Roles.FirstOrDefault(r => r.RoleID == user.RoleID);
+
+                    if (role != null)
+                    {
+                        switch (role.RoleName)
+                        {
+                            case "Admin":
+                                NavigationService.Navigate(new AdminPage(user));
+                                break;
+                            case "Client":
+                                NavigationService.Navigate(new ClientPage(user));
+                                break;
+                            default:
+                                MessageBox.Show("Неизвестная роль пользователя.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверное имя пользователя или пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
